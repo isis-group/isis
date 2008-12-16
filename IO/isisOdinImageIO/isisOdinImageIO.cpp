31282e7f4ff71d08f1f2b65abca21ea212d27938
Thomas Pröger <proeger@cbs.mpg.de>
2008-12-16 09:47:23 +0000

    Initial import

---
+++ IO/isisOdinImageIO/isisOdinImageIO.cpp
@@ -0,0 +1,44 @@
+/*
+ * C++ Implementation: isisodinimageio
+ *
+ * Description:
+ *
+ * Author: %{AUTHOR} <%{EMAIL}>, (C) 2008
+ *
+ * Copyright: See COPYING file that comes with this distribution
+ *
+ */
+
+#include "isisodinimageio.h"
+
+namespace isis {
+
+  isisOdinImageIO::isisOdinImageIO()
+    : NiftiImageIOBase() {
+  }
+
+
+  isisOdinImageIO::~isisOdinImageIO() {
+  }
+
+  bool isis::isisOdinImageIO::CanReadFile(const char *) {
+  }
+
+  bool isis::isisOdinImageIO::CanWriteFile(const char *) {
+  }
+
+  void isis::isisOdinImageIO::ReadImageInformation(void ) {
+  }
+
+  void isis::isisOdinImageIO::Read(void * buffer) {
+  }
+
+  void isis::isisOdinImageIO::WriteImageInformation(void ) {
+  }
+
+  void isis::isisOdinImageIO::Write(void * buffer) {
+  
+  }
+
+
+} // End of namespace isis
