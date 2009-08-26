3e5f107cd75bbd81fa691ac2f039a2eec8858a94
Thomas Pröger <proeger@cbs.mpg.de>
2009-08-26 14:12:43 +0000

    corrected the usage of the via command line parser

--- apps/isisReg3D/isisreg3D.cxx
+++ apps/isisReg3D/isisreg3D.cxx
@@ -5,35 +5,19 @@
  *      Author: tuerke
  */
 
-
-
 #include "extRegistration/isisRegistrationFactory3D.h"
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
 #include "itkImage.h"
 
-
 //via command parser include
 #include "viaio/option.h"
 #include "viaio/mu.h" //this is required for VNumber
+VDictEntry TYPMetric[] = { {"MattesMutualInformation", 0}, {"NormalizedHistogrammMutualInformation", 1}, {
+    "NormalizedCorrelation", 2}, {NULL}};
 
-VDictEntry TYPMetric[] = {
-		{ "MattesMutualInformation", 0},
-		{ "NormalizedHistogrammMutualInformation", 1 },
-		{ "NormalizedCorrelation", 2 },
-		{ NULL }
-};
-
-VDictEntry TYPTransform[] = {
-		{ "Rigid", 0},
-		{ "Affine", 1},
-		{ "NonLinear", 2},
-		{ "QuaternionRigid", 3},
-		{ "EulerRigid", 4},
-		{ NULL }
-};
-
-
+VDictEntry TYPTransform[] = { {"Rigid", 0}, {"Affine", 1}, {"NonLinear", 2}, {"QuaternionRigid", 3}, {"EulerRigid", 4},
+    {NULL}};
 
 //command line parser options
 VString ref_filename = NULL;
@@ -46,145 +30,134 @@ static VShort metricType = 0;
 static VShort transformType = 0;
 static VBoolean automask = false;
 
+static VOptionDescRec
+        options[] = {
+            //required inputs
+            {"ref", VStringRepn, 1, &ref_filename, VRequiredOpt, 0, "the fixed image filename"},
+            {"in", VStringRepn, 1, &in_filename, VRequiredOpt, 0, "the moving image filename"},
+            {"out", VStringRepn, 1, &out_filename, VRequiredOpt, 0, "the output image filename"},
 
-static VOptionDescRec options[] = {
-		//required inputs
-		{"ref", VStringRepn, 1, &ref_filename, VRequiredOpt, 0,
-		"the fixed image filename" },
-		{"in", VStringRepn, 1, &in_filename, VRequiredOpt, 0,
-		"the moving image filename" },
-		{"out", VStringRepn, 1, &out_filename, VRequiredOpt, 0,
-		"the output image filename" },
+            //parameter inputs
+            {"bins", VShortRepn, 1, &number_of_bins, VOptionalOpt, 0,
+                "Number of bins used by the MattesMutualInformationMetric to calculate the image histogram"},
+            {"iter", VShortRepn, 1, &number_of_iterations, VOptionalOpt, 0,
+                "Maximum number of iteration used by the optimizer"},
+            {
+                "pd",
+                VFloatRepn,
+                1,
+                &pixel_density,
+                VOptionalOpt,
+                0,
+                "The density of pixels the metric uses. 1 denotes the metric uses all pixels. Has to be > 0. Only operative with a MattesMutualInformation metric"},
+            {"automask", VBooleanRepn, 1, &automask, VOptionalOpt, 0,
+                "Creating and applying an automask using the itkOtsuThresholdFilter"},
 
-		//parameter inputs
-		{"bins", VShortRepn, 1, &number_of_bins, VOptionalOpt, 0,
-		"Number of bins used by the MattesMutualInformationMetric to calculate the image histogram"	},
-		{"iter", VShortRepn, 1, &number_of_iterations, VOptionalOpt, 0,
-		"Maximum number of iteration used by the optimizer" },
-		{"pd", VFloatRepn, 1, &pixel_density, VOptionalOpt, 0,
-		"The density of pixels the metric uses. 1 denotes the metric uses all pixels. Has to be > 0. Only operative with a MattesMutualInformation metric"},
-		{"automask", VBooleanRepn, 1, &automask, VOptionalOpt, 0,
-		"Creating and applying an automask using the itkOtsuThresholdFilter" },
+            //component inputs
+            {"metric", VShortRepn, 1, (VPointer) &metricType, VOptionalOpt, TYPMetric, "Type of the metric"}, {
+                "transform", VShortRepn, 1, (VPointer) &transformType, VOptionalOpt, TYPTransform,
+                "Type of the transform"}};
 
-		//component inputs
-		{"metric",	VShortRepn, 1, (VPointer) &metricType, VOptionalOpt, TYPMetric, "Type of the metric"},
-		{"transform", VShortRepn, 1, (VPointer) &transformType, VOptionalOpt, TYPTransform, "Type of the transform"}
-};
+int main(
+    int argc, char* argv[]) {
 
-int main( int argc, char* argv[] )
-{
-	VParseCommand( VNumber(options), options,  & argc, argv );
+	// DANGER! Kids don't try this at home! VParseCommand modifies the values of argc and argv!!!
+	if(!VParseCommand(VNumber(options), options, &argc, argv) || !VIdentifyFiles(VNumber (options), options, "in",
+	    &argc, argv, 0) || !VIdentifyFiles(VNumber (options), options, "out", &argc, argv, -1)) {
+		VReportUsage(argv[0], VNumber(options), options, NULL);
+		exit( EXIT_FAILURE);
+	}
+
+	// VParseCommand reduces the argv vector to the name of the program and  unknown command line parameters.
+	if(argc > 1) {
+		VReportBadArgs(argc, argv);
+		VReportUsage(argv[0], VNumber(options), options, NULL);
+		exit( EXIT_FAILURE);
+	}
 
 	//check pixel density
-	if( pixel_density <= 0 )
-	{
+	if(pixel_density <= 0) {
 		std::cerr << "wrong pixel density...set to 0.01" << std::endl;
 		pixel_density = 0.01;
 	}
-	if( pixel_density >= 1 )
-	{
+	if(pixel_density >= 1) {
 		std::cerr << "metric uses all pixels" << std::endl;
 	}
-
 
 	typedef short InputPixelType;
 	typedef short OutputPixelType;
 	const unsigned int Dimension = 3;
 
-	typedef itk::Image< InputPixelType, Dimension > FixedImageType;
-	typedef itk::Image< InputPixelType, Dimension > MovingImageType;
-	typedef itk::Image< OutputPixelType, Dimension > OutputImageType;
+	typedef itk::Image<InputPixelType, Dimension> FixedImageType;
+	typedef itk::Image<InputPixelType, Dimension> MovingImageType;
+	typedef itk::Image<OutputPixelType, Dimension> OutputImageType;
 
-	typedef itk::ImageFileReader< FixedImageType > FixedImageReaderType;
-	typedef itk::ImageFileReader< MovingImageType > MovingImageReaderType;
-	typedef itk::ImageFileWriter< OutputImageType > WriterType;
+	typedef itk::ImageFileReader<FixedImageType> FixedImageReaderType;
+	typedef itk::ImageFileReader<MovingImageType> MovingImageReaderType;
+	typedef itk::ImageFileWriter<OutputImageType> WriterType;
 
-	typedef isis::RegistrationFactory3D< FixedImageType, MovingImageType > RegistrationFactoryType;
+	typedef isis::RegistrationFactory3D<FixedImageType, MovingImageType> RegistrationFactoryType;
 
 	FixedImageReaderType::Pointer fixedReader = FixedImageReaderType::New();
 	MovingImageReaderType::Pointer movingReader = MovingImageReaderType::New();
 	WriterType::Pointer writer = WriterType::New();
 
-	fixedReader->SetFileName( ref_filename );
-	movingReader->SetFileName( in_filename );
-	writer->SetFileName( out_filename );
+	fixedReader->SetFileName(ref_filename);
+	movingReader->SetFileName(in_filename);
+	writer->SetFileName(out_filename);
 
 	fixedReader->Update();
 	movingReader->Update();
 
-
 	RegistrationFactoryType::Pointer registrationFactory = RegistrationFactoryType::New();
-	registrationFactory->SetInterpolator( RegistrationFactoryType::Linear );
-
+	registrationFactory->SetInterpolator(RegistrationFactoryType::Linear);
 
 	//transform setup
 	std::cout << "used transform: " << TYPTransform[transformType].keyword << std::endl;
-	switch (transformType)
-	{
+	switch(transformType) {
 	case 0:
-		registrationFactory->SetTransform( RegistrationFactoryType::VersorRigid3DTransform );
+		registrationFactory->SetTransform(RegistrationFactoryType::VersorRigid3DTransform);
 		break;
 	case 3:
-		registrationFactory->SetTransform( RegistrationFactoryType::QuaternionRigidTransform );
+		registrationFactory->SetTransform(RegistrationFactoryType::QuaternionRigidTransform);
 		break;
 	case 4:
-		registrationFactory->SetTransform( RegistrationFactoryType::CenteredEuler3DTransform );
+		registrationFactory->SetTransform(RegistrationFactoryType::CenteredEuler3DTransform);
 		break;
 	}
 
 	//metric setup
 	std::cout << "used metric: " << TYPMetric[metricType].keyword << std::endl;
-	switch (metricType)
-	{
+	switch(metricType) {
 	case 0:
-		registrationFactory->SetMetric( RegistrationFactoryType::MattesMutualInformation );
+		registrationFactory->SetMetric(RegistrationFactoryType::MattesMutualInformation);
 		break;
 
 	case 1:
-		registrationFactory->SetMetric( RegistrationFactoryType::NormalizedMutualInformation );
+		registrationFactory->SetMetric(RegistrationFactoryType::NormalizedMutualInformation);
 		break;
 
 	case 2:
-		registrationFactory->SetMetric( RegistrationFactoryType::NormalizedCorrelation );
+		registrationFactory->SetMetric(RegistrationFactoryType::NormalizedCorrelation);
 		break;
 	}
 
+	if(automask) {
+		registrationFactory->UserOptions.USEOTSUTHRESHOLDING = true;
+	}
 
-
-	if( automask ) { registrationFactory->UserOptions.USEOTSUTHRESHOLDING = true; }
-
-
-
-
-
-
-	registrationFactory->SetOptimizer( RegistrationFactoryType::VersorRigidOptimizer );
+	registrationFactory->SetOptimizer(RegistrationFactoryType::VersorRigidOptimizer);
 	registrationFactory->UserOptions.NumberOfIterations = number_of_iterations;
 	registrationFactory->UserOptions.NumberOfBins = number_of_bins;
 	registrationFactory->UserOptions.PixelDensity = pixel_density;
 	registrationFactory->UserOptions.PRINTRESULTS = true;
 
-
-	registrationFactory->SetFixedImage( fixedReader->GetOutput() );
-	registrationFactory->SetMovingImage( movingReader->GetOutput() );
+	registrationFactory->SetFixedImage(fixedReader->GetOutput());
+	registrationFactory->SetMovingImage(movingReader->GetOutput());
 	registrationFactory->StartRegistration();
-	writer->SetInput( registrationFactory->GetRegisteredImage() );
+	writer->SetInput(registrationFactory->GetRegisteredImage());
 	writer->Update();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 	return 0;
-
 
 }
