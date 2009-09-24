c3eef6eed1dd112de14e02f3050bffd4c72ee681
Thomas Pröger <proeger@cbs.mpg.de>
2009-09-24 14:37:50 +0000

    The first unit test cases, implemented with BOOST.Test

---
+++ tests/CoreUtils/propertyTest.cpp
@@ -0,0 +1,32 @@
+/*
+ * propertyTest.cpp
+ *
+ *  Created on: Sep 23, 2009
+ *      Author: proeger
+ */
+
+// The BOOST_TEST_MODULE is similar to a unit test suite.
+#define BOOST_TEST_MODULE PropertyTests
+#include <boost/test/included/unit_test.hpp>
+#include <CoreUtils/property.hpp>
+#include <string>
+#include <iostream>
+
+BOOST_AUTO_TEST_CASE(property_init_test) {
+
+	//	default constructor
+	isis::util::Property propZero;
+
+	//	initializer
+	isis::util::Property propA = std::string("Property01");
+	BOOST_CHECK(propA->toString().compare("Property01") == 0);
+
+	//	copy constructor
+	isis::util::Property propB = propA;
+	BOOST_CHECK(propB->toString().compare("Property01") == 0);
+
+	//	default: not needed
+	BOOST_CHECK(!propA.needed());
+	BOOST_CHECK(!propZero.needed());
+
+}
