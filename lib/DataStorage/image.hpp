b72e69cd94f0dc01ac4e371657bf5f0656becb03
Thomas Pröger <proeger@cbs.mpg.de>
2009-10-01 08:15:07 +0000

    The basic interface of an Image object.

--- lib/DataStorage/image.hpp
+++ lib/DataStorage/image.hpp
@@ -25,16 +25,98 @@
 #include <list>
 #include <boost/shared_ptr.hpp>
 
-namespace isis{ namespace data{ 
-class Image {
+namespace isis{ namespace data
+{
+
+class Image
+{
+
+public:
+
+	/**
+	 * This method returns a reference to the voxel value at the given coordinates.
+	 *
+	 * The voxel reference provides reading and writing access to the refered
+	 * value.
+	 *
+	 * \param first The first coordinate in voxel space. Usually the x value.
+	 * \param second The second coordinate in voxel space. Usually the y value.
+	 * \param third The third coordinate in voxel space. Ususally the z value.
+	 * \param fourth The fourth coordinate in voxel space. Usually the time value.
+	 *
+	 * \return A reference to the addressed voxel value. Reading and writing access
+	 * is provided.
+	 */
+	template <typename T> T& voxel(
+		const size_t &first,
+		const size_t &second,
+		const size_t &third,
+		const size_t &fourth);
+
+	/**
+	 * This method returns a reference to the voxel value at the given coordinates.
+	 *
+	 * The const voxel reference provides only reading access to the refered
+	 * value.
+	 *
+	 * \param first The first coordinate in voxel space. Usually the x value.
+	 * \param second The second coordinate in voxel space. Usually the y value.
+	 * \param third The third coordinate in voxel space. Ususally the z value.
+	 * \param fourth The fourth coordinate in voxel space. Usually the time value.
+	 *
+	 * \return A reference to the addressed voxel value. Only reading access is provided
+	 */
+	template <typename T> T& voxel(
+		const size_t &first,
+		const size_t &second,
+		const size_t &third,
+		const size_t &fourth)const;
+
+	/**
+	 * Returns a copy of the chunk that contains the voxel at the given coordinates.
+	 *
+	 * \param first The first coordinate in voxel space. Usually the x value.
+	 * \param second The second coordinate in voxel space. Usually the y value.
+	 * \param third The third coordinate in voxel space. Ususally the z value.
+	 * \param fourth The fourth coordinate in voxel space. Usually the time value.
+	 *
+	 *
+	 */
+	template <typename T> Chunk<T> getChunk(
+		const size_t &first,
+		const size_t &second,
+		const size_t &third,
+		const size_t &fourth)const;
+
+	/**
+	 * This method merges the content of a ChunkList eith the Image's internal chunks.
+	 *
+	 * The given ChunkList will be traversed and each chunk will be inserted in the right
+	 * place according to its properties. The chunks properties must be set correctly
+	 * to match the image's voxel sort order.
+	 *
+	 * Maybe in the near future, this function throws an exception.
+	 *
+	 * \param chunks A list of chunks that should be merged with the image's internal
+	 *  chunk list.
+	 */
+	void insertChunkList(const ChunkList &chunks);
+
 };
 
 /// @cond _internal
 namespace _internal{
-class ImageList : public std::list< boost::shared_ptr<Image> >{
+class ImageList : public std::list< boost::shared_ptr<Image> >
+{
 public:
-	ImageList(const ChunkList &src);
+
+	/**
+	 * Create a number of images out of a Chunk list.
+	 */
+	ImageList(const ChunkList src);
+
 };
+
 }
 /// @endcond
 }}
