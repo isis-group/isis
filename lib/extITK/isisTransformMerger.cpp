97efd2c57458c39286c94d0d96f19732312d2bb3
Erik Türke <tuerke@cbs.mpg.de>
2009-11-04 09:26:59 +0000

    isisTransformMerger is now able to merger transforms. It creates a vector deformation field which includes all transforms passed to the transfoem merger

--- lib/extITK/isisTransformMerger.cpp
+++ lib/extITK/isisTransformMerger.cpp
@@ -13,42 +13,68 @@ namespace isis {
 namespace extitk {
 
 TransformMerger::TransformMerger() {
-	outputType = 0;
-	tmpTransform_ = BSplineDeformableTransformType::New();
-	outputTransform_ = BSplineDeformableTransformType::New();
+    transformType_ = 0;
+    tmpTransform_ = BSplineDeformableTransformType::New();
+    outputTransform_ = BSplineDeformableTransformType::New();
+	addImageFilter_ = AddImageFilterType::New();
 
 }
 
 void TransformMerger::merge(
     void) {
-
-	for(transformIterator_ = this->begin(); transformIterator_ != this->end(); transformIterator_++) {
-
-		if(VersorRigid3DTransformType* transform = dynamic_cast<VersorRigid3DTransformType*> (*transformIterator_)) {
-			outputType = 1;
-			std::cout << transform->GetParameters() << std::endl;
-
-			std::cout << tmpTransform_->GetParameters() << std::endl;
-			std::cout << tmpTransform_->GetNumberOfParameters() << std::endl;
-			//TODO versor rigid 3d type
+	
+    DeformationFieldIteratorType fi(temporaryDeformationField_, imageRegion_);
+	itk::Transform<double, 3,3>::InputPointType fixedPoint;
+	itk::Transform<double, 3,3>::OutputPointType movingPoint;
+	DeformationFieldType::IndexType index;
+	VectorType displacement;
+	
+    //go through the transform list and create a vector deformation field (temporaryDeformationField_). Then combining this deformation field with the final vector field (deformationField_).
+    for (transformIterator_ = this->begin(); transformIterator_ != this->end(); transformIterator_++) {
+        fi.GoToBegin();
+		itk::Transform<double, 3, 3>* transform =  dynamic_cast<itk::Transform<double, 3, 3>* >(*transformIterator_);
+		while(!fi.IsAtEnd())
+		{
+			index = fi.GetIndex();
+			temporaryDeformationField_->TransformIndexToPhysicalPoint(index, fixedPoint);
+			movingPoint = transform->TransformPoint(fixedPoint);
+			displacement = movingPoint - fixedPoint;
+			fi.Set(displacement);
+			++fi;	
 		}
-		if(AffineTransformType* transform = dynamic_cast<AffineTransformType*> (*transformIterator_)) {
-			//TODO affine rigid 3d type
-			outputType = 2;
-			std::cout << transform->GetParameters() << std::endl;
+		addImageFilter_->SetInput1(deformationField_);
+		addImageFilter_->SetInput2(temporaryDeformationField_);
+		addImageFilter_->Update();
+		deformationField_ = addImageFilter_->GetOutput();
 
-		}
-		if(BSplineDeformableTransformType* transform = dynamic_cast<BSplineDeformableTransformType*> (*transformIterator_)) {
-			//TODO deformable transform type
-
-		}
-	}
+    }
 
 }
 
-TransformBasePointer TransformMerger::getTransform(
+TransformMerger::DeformationFieldType::Pointer TransformMerger::getTransform(
     void) {
+    return deformationField_;
 
+}
+
+//here we setting up the temporaryDeformationField_ and deformationField_. The properties are defined be the templateImage which is specified by the setTemplateImage method,
+void TransformMerger::setTemplateImage(itk::ImageBase<3>::Pointer templateImage)
+{
+    imageRegion_ = templateImage->GetLargestPossibleRegion();
+    deformationField_ = DeformationFieldType::New();
+    deformationField_->SetRegions(imageRegion_.GetSize());
+    deformationField_->SetOrigin(templateImage->GetOrigin());
+    deformationField_->SetSpacing(templateImage->GetSpacing());
+    deformationField_->SetDirection(templateImage->GetDirection());
+    deformationField_->Allocate();
+
+    temporaryDeformationField_ = DeformationFieldType::New();
+    temporaryDeformationField_->SetRegions(imageRegion_.GetSize());
+    temporaryDeformationField_->SetOrigin(templateImage->GetOrigin());
+    temporaryDeformationField_->SetSpacing(templateImage->GetSpacing());
+    temporaryDeformationField_->SetDirection(templateImage->GetDirection());
+    temporaryDeformationField_->Allocate();
+	
 }
 
 }//end namespace extitk
--- lib/extITK/isisTransformMerger.hpp
+++ lib/extITK/isisTransformMerger.hpp
@@ -10,6 +10,7 @@
 #include "itkAffineTransform.h"
 #include "itkBSplineDeformableTransform.h"
 #include "itkImageRegistrationMethod.h"
+#include "itkAddImageFilter.h"
 
 #include <list>
 
@@ -21,33 +22,48 @@ typedef itk::TransformBase* TransformBasePointer;
 class TransformMerger : public std::list<TransformBasePointer>
 {
 public:
+	
+    typedef TransformMerger Self;
+    typedef std::list<TransformBasePointer> Superclass;
 
-	typedef TransformMerger Self;
-	typedef std::list<TransformBasePointer> Superclass;
+    typedef itk::Vector<float, 3> VectorType;
+    typedef itk::Image<VectorType, 3> DeformationFieldType;
+    typedef itk::ImageRegionIterator<DeformationFieldType> DeformationFieldIteratorType;
 
-	typedef itk::MatrixOffsetTransformBase<double, 3, 3> MatrixOffsetTransformType;
+    typedef itk::MatrixOffsetTransformBase<double, 3, 3> MatrixOffsetTransformType;
 
-	typedef itk::VersorRigid3DTransform<double> VersorRigid3DTransformType;
-	typedef itk::AffineTransform<double, 3> AffineTransformType;
-	typedef itk::BSplineDeformableTransform<double, 3, 3> BSplineDeformableTransformType;
+    typedef itk::VersorRigid3DTransform<double> VersorRigid3DTransformType;
+    typedef itk::AffineTransform<double, 3> AffineTransformType;
+    typedef itk::BSplineDeformableTransform<double, 3, 3> BSplineDeformableTransformType;
+	
+	typedef itk::AddImageFilter<DeformationFieldType, DeformationFieldType, DeformationFieldType> AddImageFilterType;
 
-	TransformMerger();
 
-	void merge(
-	    void);
+    TransformMerger();
 
-	TransformBasePointer getTransform(
-	    void);
+    void merge(
+        void);
+
+    DeformationFieldType::Pointer getTransform(
+        void);
+
+    void setTemplateImage(itk::ImageBase<3>::Pointer);
 
 private:
-	unsigned int outputType;
-	Self::iterator transformIterator_;
-	MatrixOffsetTransformType::MatrixType temporaryMatrix_;
-	MatrixOffsetTransformType::TranslationType temporaryTranslation_;
-	MatrixOffsetTransformType::OffsetType temporaryOffset_;
+    unsigned int transformType_;
+    Self::iterator transformIterator_;
+    MatrixOffsetTransformType::MatrixType temporaryMatrix_;
+    MatrixOffsetTransformType::TranslationType temporaryTranslation_;
+    MatrixOffsetTransformType::OffsetType temporaryOffset_;
 
-	BSplineDeformableTransformType::Pointer tmpTransform_;
-	BSplineDeformableTransformType::Pointer outputTransform_;
+    BSplineDeformableTransformType::Pointer tmpTransform_;
+    BSplineDeformableTransformType::Pointer outputTransform_;
+    
+    DeformationFieldType::Pointer deformationField_;
+    DeformationFieldType::Pointer temporaryDeformationField_;
+    itk::ImageBase<3>::RegionType imageRegion_;
+	
+	AddImageFilterType::Pointer addImageFilter_;
 
 };
 
