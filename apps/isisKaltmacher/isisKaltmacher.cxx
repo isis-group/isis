daa584444af96f0bed5278e4706fc18770568589
Thomas Pröger <proeger@cbs.mpg.de>
2009-11-27 15:26:29 +0000

    isisKaltmacher now supports an '-out' option to write transformation data to disk.

--- apps/isisKaltmacher/isisKaltmacher.cxx
+++ apps/isisKaltmacher/isisKaltmacher.cxx
@@ -14,6 +14,7 @@
 //itk related includes
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
+#include "itkTransformFileWriter.h"
 
 #include "itkImageRegistrationMethod.h"
 #include "itkVersorRigid3DTransform.h"
@@ -22,13 +23,14 @@
 #include "itkLinearInterpolateImageFunction.h"
 
 #include "extRegistration/isisIterationObserver.h"
+#include <iostream>
 
 //command line parser options
 static VString ref_filename = NULL;
 static VString in_filename = NULL;
-static VBoolean resample = false;
+//static VBoolean resample = false;
 static VString out_filename = NULL;
-static VBoolean in_found, ref_found;
+static VBoolean in_found, ref_found, out_found;
 
 //optimizer related parser options
 static VArgVector scales;
@@ -43,6 +45,7 @@ options[] = {
     //required inputs
     {"ref", VStringRepn, 1, &ref_filename, &ref_found, 0, "the fixed image filename"},
     {"in", VStringRepn, 1, &in_filename, &in_found, 0, "the moving image filename"},
+    {"out", VStringRepn, 1, &out_filename, &out_found, 0, "the output transformation filename"},
     {"scales", VFloatRepn, 0, (VPointer) &scales, VOptionalOpt, 0, "vector of the scales"},
     {"minStep", VFloatRepn, 1, &minStep, VOptionalOpt, 0, ""},
     {"maxStep", VFloatRepn, 1, &maxStep, VOptionalOpt, 0, ""},
@@ -70,101 +73,99 @@ int main(int argc, char* argv[])
         VReportUsage(argv[0], VNumber(options), options, NULL);
         exit(1);
     }
-  
-  if(scales.number < 6)
-  {
-    std::cout << "the number of scales has to be 6!" << std::endl;
-    return EXIT_FAILURE;
-  }
-  
-  const unsigned int Dimension = 3;
-  typedef signed short PixelType;
-  
-  typedef itk::Image<PixelType, Dimension> ImageType;
-  typedef itk::ImageFileReader<ImageType> ImageFileReaderType;
-  typedef itk::ImageFileWriter<ImageType> ImageFileWriterType;
-  
-  typedef itk::ImageRegistrationMethod<ImageType, ImageType> ImageRegistrationMethodType;
-  typedef itk::LinearInterpolateImageFunction<ImageType, double> InterpolateImageFunctionType;
-  typedef itk::MattesMutualInformationImageToImageMetric<ImageType, ImageType> MattesMutualInformationImageToImageMetricType;
-  typedef itk::VersorRigid3DTransformOptimizer VersorRigid3DTransformOptimizerType;
-  typedef itk::VersorRigid3DTransform<double> VersorRigid3DTransformType;
-  
-  ImageFileReaderType::Pointer fixedReader = ImageFileReaderType::New();
-  ImageFileReaderType::Pointer movingReader = ImageFileReaderType::New();
-  ImageFileWriterType::Pointer writer = ImageFileWriterType::New();
-  
-  ImageRegistrationMethodType::Pointer registrationMethod = ImageRegistrationMethodType::New();
-  InterpolateImageFunctionType::Pointer interpolator = InterpolateImageFunctionType::New();
-  MattesMutualInformationImageToImageMetricType::Pointer metric = MattesMutualInformationImageToImageMetricType::New();
-  VersorRigid3DTransformOptimizerType::Pointer optimizer = VersorRigid3DTransformOptimizerType::New();
-  VersorRigid3DTransformType::Pointer transform = VersorRigid3DTransformType::New();
- 
-  isis::IterationObserver::Pointer observer = isis::IterationObserver::New();
-  
-  fixedReader->SetFileName(ref_filename);
-  movingReader->SetFileName(in_filename);
-  fixedReader->Update();
-  movingReader->Update();
-  
-  //set up the registrion method itself
-  registrationMethod->SetFixedImage(fixedReader->GetOutput());
-  registrationMethod->SetMovingImage(movingReader->GetOutput());
-  registrationMethod->SetFixedImageRegion(fixedReader->GetOutput()->GetLargestPossibleRegion());
-  registrationMethod->SetInterpolator(interpolator);
-  registrationMethod->SetMetric(metric);
-  registrationMethod->SetOptimizer(optimizer);
-  registrationMethod->SetTransform(transform);
-  registrationMethod->SetInitialTransformParameters(transform->GetParameters());
-  
-  //set up the optimizer
-  
-  VersorRigid3DTransformOptimizerType::ScalesType optimizerScale(transform->GetNumberOfParameters());
-  for(unsigned int i = 0; i<6; i++)
-  {
-    
-    optimizerScale[i] = ((VFloat*) scales.vector)[i];
-    
-  }
- 
-  optimizer->SetScales(optimizerScale);
-  optimizer->SetGradientMagnitudeTolerance(gmt);
-  optimizer->SetMaximumStepLength(maxStep);
-  optimizer->SetMinimumStepLength(minStep);
-  optimizer->SetNumberOfIterations(iter);
-  optimizer->SetRelaxationFactor(relax);
-  
-  //set up the metric
-  metric->SetFixedImage(fixedReader->GetOutput());
-  metric->SetMovingImage(movingReader->GetOutput());
-  metric->SetFixedImageRegion(fixedReader->GetOutput()->GetLargestPossibleRegion());
-  metric->SetNumberOfHistogramBins(50);
-  metric->SetNumberOfSpatialSamples(fixedReader->GetOutput()->GetLargestPossibleRegion().GetNumberOfPixels()/100);
-  metric->ReinitializeSeed(1);
-  
-  
-  optimizer->AddObserver(itk::IterationEvent(), observer);
-  
-  registrationMethod->StartRegistration();
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  return 0;
+
+    if(scales.number < 6)
+    {
+    	std::cout << "the number of scales has to be 6!" << std::endl;
+    	return EXIT_FAILURE;
+    }
+
+    const unsigned int Dimension = 3;
+    typedef signed short PixelType;
+
+    typedef itk::Image<PixelType, Dimension> ImageType;
+	typedef itk::ImageFileReader<ImageType> ImageFileReaderType;
+	typedef itk::ImageFileWriter<ImageType> ImageFileWriterType;
+
+	typedef itk::ImageRegistrationMethod<ImageType, ImageType> ImageRegistrationMethodType;
+	typedef itk::LinearInterpolateImageFunction<ImageType, double> InterpolateImageFunctionType;
+	typedef itk::MattesMutualInformationImageToImageMetric<ImageType, ImageType>
+	                MattesMutualInformationImageToImageMetricType;
+	typedef itk::VersorRigid3DTransformOptimizer VersorRigid3DTransformOptimizerType;
+	typedef itk::VersorRigid3DTransform<double> VersorRigid3DTransformType;
+
+	//  Transformation
+	itk::TransformFileWriter::Pointer transformWriter = itk::TransformFileWriter::New();
+	const itk::TransformBase* tmpTransform;
+
+	ImageFileReaderType::Pointer fixedReader = ImageFileReaderType::New();
+	ImageFileReaderType::Pointer movingReader = ImageFileReaderType::New();
+	ImageFileWriterType::Pointer writer = ImageFileWriterType::New();
+
+	ImageRegistrationMethodType::Pointer registrationMethod = ImageRegistrationMethodType::New();
+	InterpolateImageFunctionType::Pointer interpolator = InterpolateImageFunctionType::New();
+	MattesMutualInformationImageToImageMetricType::Pointer metric =
+	                MattesMutualInformationImageToImageMetricType::New();
+	VersorRigid3DTransformOptimizerType::Pointer optimizer =
+	                VersorRigid3DTransformOptimizerType::New();
+	VersorRigid3DTransformType::Pointer transform = VersorRigid3DTransformType::New();
+
+	isis::IterationObserver::Pointer observer = isis::IterationObserver::New();
+
+	fixedReader->SetFileName(ref_filename);
+	movingReader->SetFileName(in_filename);
+	fixedReader->Update();
+	movingReader->Update();
+
+	//set up the registrion method itself
+	registrationMethod->SetFixedImage(fixedReader->GetOutput());
+	registrationMethod->SetMovingImage(movingReader->GetOutput());
+	registrationMethod->SetFixedImageRegion(fixedReader->GetOutput()->GetLargestPossibleRegion());
+	registrationMethod->SetInterpolator(interpolator);
+	registrationMethod->SetMetric(metric);
+	registrationMethod->SetOptimizer(optimizer);
+	registrationMethod->SetTransform(transform);
+	registrationMethod->SetInitialTransformParameters(transform->GetParameters());
+
+	//set up the optimizer
+
+  VersorRigid3DTransformOptimizerType::ScalesType optimizerScale(
+	    transform->GetNumberOfParameters());
+	for(unsigned int i = 0; i < 6; i++)
+	{
+
+		optimizerScale[i] = ((VFloat*) scales.vector)[i];
+
+	}
+
+	optimizer->SetScales(optimizerScale);
+	optimizer->SetGradientMagnitudeTolerance(gmt);
+	optimizer->SetMaximumStepLength(maxStep);
+	optimizer->SetMinimumStepLength(minStep);
+	optimizer->SetNumberOfIterations(iter);
+	optimizer->SetRelaxationFactor(relax);
+
+	//set up the metric
+	metric->SetFixedImage(fixedReader->GetOutput());
+	metric->SetMovingImage(movingReader->GetOutput());
+	metric->SetFixedImageRegion(fixedReader->GetOutput()->GetLargestPossibleRegion());
+	metric->SetNumberOfHistogramBins(50);
+	metric->SetNumberOfSpatialSamples(fixedReader->GetOutput()->GetLargestPossibleRegion().GetNumberOfPixels() / 100);
+	metric->ReinitializeSeed(1);
+
+	optimizer->AddObserver(itk::IterationEvent(), observer);
+	registrationMethod->StartRegistration();
+
+	//  if output filename found -> save transformation to disk
+	if(out_filename && out_found)
+	{
+		std::cout << "Writing transformation to disk" << std::endl;
+
+		tmpTransform = registrationMethod->GetTransform();
+		transformWriter->SetInput(tmpTransform);
+		transformWriter->SetFileName(out_filename);
+		transformWriter->Update();
+	}
+
+	return 0;
 };
