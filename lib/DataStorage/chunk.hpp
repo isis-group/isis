ce84d180b5319aea5a5c1a602ababb90ffef045b
Lydia Hellrung <hellrung@cbs.mpg.de>
2010-01-23 18:22:39 +0000

    spelling mistake in chunk_comparison changed

--- lib/DataStorage/chunk.hpp
+++ lib/DataStorage/chunk.hpp
@@ -102,9 +102,9 @@ public:
 /// @cond _internal
 namespace _internal{
 
-struct chunk_comarison : public std::binary_function< Chunk, Chunk, bool>{
+struct chunk_comparison : public std::binary_function< Chunk, Chunk, bool>{
 	virtual bool operator() (const Chunk& a, const Chunk& b)const=0;
-	virtual ~chunk_comarison(){}
+	virtual ~chunk_comparison(){}
 };
 }
 /// @endcond
