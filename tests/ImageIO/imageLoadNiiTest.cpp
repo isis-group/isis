19fc42937eff5df36dea0fc428ff5167e8fe58d2
Lydia Hellrung <hellrung@cbs.mpg.de>
2010-02-23 13:38:04 +0000

    introduction nifti chunk for whole file handling acquisition number as needed

--- tests/ImageIO/imageLoadNiiTest.cpp
+++ tests/ImageIO/imageLoadNiiTest.cpp
@@ -19,13 +19,14 @@ int main(int argc, char *argv[]){
 
 	ENABLE_LOG(isis::data::DataDebug,isis::util::DefaultMsgPrint,isis::util::info);
 	ENABLE_LOG(isis::data::DataLog,isis::util::DefaultMsgPrint,isis::util::info);
-//	ENABLE_LOG(isis::util::CoreDebug,isis::util::DefaultMsgPrint,isis::util::info);
-//	ENABLE_LOG(isis::util::CoreLog,isis::util::DefaultMsgPrint,isis::util::info);
+	ENABLE_LOG(isis::util::CoreDebug,isis::util::DefaultMsgPrint,isis::util::info);
+	ENABLE_LOG(isis::util::CoreLog,isis::util::DefaultMsgPrint,isis::util::info);
 	ENABLE_LOG(isis::image_io::ImageIoDebug,isis::util::DefaultMsgPrint,isis::util::info);
 	ENABLE_LOG(isis::image_io::ImageIoLog,isis::util::DefaultMsgPrint,isis::util::info);
 
-	isis::data::ImageList img = isis::data::IOFactory::load("/SCR/EXAMPLE/EXAMPLE1/SPM/FR1T/functional/data.nii", "");
+	isis::data::ImageList img = isis::data::IOFactory::load("/tmp/data.nii", "");
 
-	isis::data::IOFactory::write(img, "nifti_read_test.nii", "");
+	/*/SCR/Programming2/hellrung/isis/Debug/tests/ImageIO/*/
+	isis::data::IOFactory::write(img, "/tmp/nifti_write_test.nii", "");
 	return EXIT_SUCCESS;
 }
