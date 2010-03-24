30530090c348bd25544ce79e318f585d58cac8d6
Enrico Reimer <reimer@cbs.mpg.de>
2010-03-24 11:54:06 +0000

    EN adapted singleton test

--- lib/CoreUtils/singletons.hpp
+++ lib/CoreUtils/singletons.hpp
@@ -4,6 +4,7 @@
 #include <map>
 #include <boost/noncopyable.hpp>
 #include <boost/shared_ptr.hpp>
+#include <boost/weak_ptr.hpp>
 #include <string>
 #include <iostream>
 #include <typeinfo>
--- tests/CoreUtils/singletonTest.cpp
+++ tests/CoreUtils/singletonTest.cpp
@@ -17,12 +17,12 @@ public:
 using namespace isis::util;
 using namespace isis::test;
 int main(){
-	SingleTest<1> &s1= Singletons::get<SingleTest<1>,10>();
-	if(&s1!= &Singletons::get<SingleTest<1>,0>())
+	boost::shared_ptr<SingleTest<1> > s1= Singletons::get<SingleTest<1>,10>();
+	if(s1!= Singletons::get<SingleTest<1>,0>())
 		std::cout << "Second request for SingleTest<1> differs" << std::endl;
 
-	if((void*)&s1== (void*)&Singletons::get<SingleTest<2>,5>())
+	if((void*)s1.get()== (void*)Singletons::get<SingleTest<2>,5>().get())
 		std::cout << "request for SingleTest<2> gets Singleton1" << std::endl;
-	if((void*)&s1== (void*)&Singletons::get<SingleTest<3>,5>()) // this should be deleted before SingleTest<2>
+	if((void*)s1.get()== (void*)Singletons::get<SingleTest<3>,5>().get()) // this should be deleted before SingleTest<2>
 		std::cout << "request for SingleTest<3> gets Singleton1" << std::endl;
 }
