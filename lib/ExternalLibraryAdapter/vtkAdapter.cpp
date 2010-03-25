a2d552843b563cac82bca40d70e6acc1ac6fa54f
Erik Türke <tuerke@cbs.mpg.de>
2010-03-25 07:25:40 +0000

    Fixed CmakeLists.txt

--- lib/ExternalLibraryAdapter/vtkAdapter.cpp
+++ lib/ExternalLibraryAdapter/vtkAdapter.cpp
@@ -5,15 +5,15 @@ namespace isis{ namespace adapter{
 VTKAdapter::VTKAdapter(const boost::shared_ptr<isis::data::Image> src )
     : vtkImageData(), dimensions(src->sizeToVector())
 {
-    //go through all the chunks and check for consistent data type
+    //go through all the chunks and check for consistent datatype
     unsigned int firstTypeID = src->chunksBegin()->typeID();
     for (data::Image::ChunkIterator ci = src->chunksBegin();ci != src->chunksEnd(); *ci++)
     {
 	if(not ci->typeID() == firstTypeID)
 	{
-	    LOG(DataLog, error) << "Inconsistent ";
+	    LOG(DataLog, error) << "Inconsistent chunk datatype!";
+	    delete this; //TODO exception handle in constructor ??
 	}
-	
     }
     
     
