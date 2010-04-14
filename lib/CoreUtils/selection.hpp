706c618238809e7b3e540d3336db67bb75e04e30
Enrico Reimer <reimer@cbs.mpg.de>
2010-04-14 14:51:17 +0000

    EN added documentation for Selection

--- lib/CoreUtils/selection.hpp
+++ lib/CoreUtils/selection.hpp
@@ -14,26 +14,64 @@
 
 namespace isis{ namespace util{
 
+/**
+ * Enum-like class for string based selections. 
+ * The options are NOT case sensitive.
+ */	
 class Selection
 {
 	typedef std::map<std::string,int,_internal::caselessStringLess> map_type;
 	map_type ent_map;
 	int m_set;
 public:
+	/**
+	 * Default constructor. 
+	 * Creates a selection with the given options.
+	 * \param entries comma separated of the options as a string
+	 */
 	Selection(const char *entries);
+	/// Fallback contructor to enable creation of empty selections
 	Selection();
+	/**
+	 * Set the selection to the given type.
+	 * If the given option does not exist, a runtime error will be send and the selection won't be set.
+	 * \param entry the option the selection should be set to.
+	 * \returns true if the option was set, false otherwise.
+	 */
 	bool set(const char* entry);
+	/**
+	 * Implicit cast to int.
+	 * The numbers correspont to the order the options where given at the creation of the selection (first option -> 1, second option -> 2 ...)
+	 * \returns number corresponding the currently set option or "0" if none is set
+	 */
 	operator const int()const;
+	/**
+	 * Implicit cast to string.
+	 * \returns the currently set option or "<<NOT_SET>>" if none is set
+	 */
 	operator const std::string()const;
+	/**
+	 * Common comparison.
+	 * \returns true if both selection have the same options and are currently set to the the option. False otherwise.
+	 */
 	bool operator==(const Selection &ref)const;
+	/**
+	 * String comparison.
+	 * \returns true if the currently set option is non-case-sensitive equal to the given string. False otherwise.
+	 */
 	bool operator==(const char ref[])const;
+	/**
+	 * Number comparison.
+	 * \returns true if the number corresponding the currently set option is equal to the given number. False otherwise.
+	 */
 	bool operator==(const int ref)const;
-	
+	/// \returns a list of all options
 	std::list<std::string> getEntries()const;
 };
 }}
 
 namespace std{
+/// Streaming output for selections.
 template<typename charT, typename traits>
 basic_ostream<charT,traits> &operator<<(basic_ostream<charT,traits> &out,const isis::util::Selection &s)
 {
