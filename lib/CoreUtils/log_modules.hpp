aa8ea4cc80351f2aa69c413452c148c47e45131d
Enrico Reimer <reimer@cbs.mpg.de>
2010-08-11 12:36:41 +0000

    EN rename the log-modules which are enabled by DEBUG_LOG to *Debug to distinguish them from the "Runtime"-logs (closes #95)

--- lib/CoreUtils/log_modules.hpp
+++ lib/CoreUtils/log_modules.hpp
@@ -5,13 +5,13 @@
 namespace isis
 {
 struct CoreLog {static const char *name() {return "Core";}; enum {use = _ENABLE_LOG};};
-struct CoreDebug {static const char *name() {return "Core";}; enum {use = _ENABLE_DEBUG};};
+struct CoreDebug {static const char *name() {return "CoreDebug";}; enum {use = _ENABLE_DEBUG};};
 
 struct ImageIoLog {static const char *name() {return "ImageIO";}; enum {use = _ENABLE_LOG};};
-struct ImageIoDebug {static const char *name() {return "ImageIO";}; enum {use = _ENABLE_DEBUG};};
+struct ImageIoDebug {static const char *name() {return "ImageIODebug";}; enum {use = _ENABLE_DEBUG};};
 
 struct DataLog {static const char *name() {return "Data";}; enum {use = _ENABLE_LOG};};
-struct DataDebug {static const char *name() {return "Data";}; enum {use = _ENABLE_DEBUG};};
+struct DataDebug {static const char *name() {return "DataDebug";}; enum {use = _ENABLE_DEBUG};};
 }
 /// @endcond
 
