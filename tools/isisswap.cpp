d4afca694bd5db8a8dbe88891f5f9fefd18bd514
Erik Türke <tuerke@cbs.mpg.de>
2010-08-13 15:57:40 +0000

    isisswap now can swap all image types

--- tools/isisswap.cpp
+++ tools/isisswap.cpp
@@ -7,7 +7,7 @@
 using namespace isis;
 
 template<typename TYPE>
-data::Image voxelSwapZ( const boost::shared_ptr<data::Image> src )
+data::Image voxelSwapZ( const boost::shared_ptr<data::Image> src, unsigned int dim )
 {
 	data::MemImage<TYPE> dest( *src );
 	data::Image tmpImage = dest;
@@ -15,8 +15,10 @@ data::Image voxelSwapZ( const boost::shared_ptr<data::Image> src )
 		for ( size_t z = 0; z < src->sizeToVector()[2]; z++ ) {
 			for ( size_t y = 0; y < src->sizeToVector()[1]; y++ ) {
 				for ( size_t x = 0; x < src->sizeToVector()[0]; x++ ) {
-					tmpImage.voxel<TYPE>(x, y, z, t) = src->voxel<TYPE>(x, y, (src->sizeToVector()[2] - z)-1, t);
-					std::cout << tmpImage.voxel<TYPE>(x, y, z, t);
+					tmpImage.voxel<TYPE>(x, y, z, t) = src->voxel<TYPE>(dim==0 ? (src->sizeToVector()[0] - x)-1 : x,
+																		dim==1 ? (src->sizeToVector()[1] - y)-1 : y,
+																		dim==2 ? (src->sizeToVector()[2] - z)-1 : z,
+																		t);
 				}
 			}
 		}
@@ -50,11 +52,6 @@ int main( int argc, char **argv )
 	unsigned int dim = alongMap[app.parameters["along"].toString()];
 	//go through every image
 	BOOST_FOREACH( data::ImageList::const_reference refImage, app.images ) {
-		//if we have a 4d image not organized at least as dim[3] chunks we have to splice
-		//down to the timeDim. Now we have at least 3d images we can transform
-		if( refImage->sizeToVector()[3] > refImage->getChunkList().size() ) {
-			refImage->spliceDownTo(data::timeDim);
-		}
 		//map from pyhisical into image space
 		if(dim == 3) {
 			dim = getBiggestVecElem( refImage->getProperty<util::fvector4>("readVec") );
@@ -65,84 +62,68 @@ int main( int argc, char **argv )
 		if(dim == 5) {
 			dim = getBiggestVecElem( refImage->getProperty<util::fvector4>("sliceVec") );
 		}
-		data::Image newImage;
-		if ( refImage->getChunkAt(0).relevantDims() < 3 && dim == 2 ) {
+		boost::numeric::ublas::matrix<float> T( 3, 3 );
+		T( 0, 0 ) = 1;
+		T( 0, 1 ) = 0;
+		T( 0, 2 ) = 0;
+		T( 1, 0 ) = 0;
+		T( 1, 1 ) = 1;
+		T( 1, 2 ) = 0;
+		T( 2, 0 ) = 0;
+		T( 2, 1 ) = 0;
+		T( 2, 2 ) = 1;
+		T( dim, dim ) *= -1;
+		data::Image newImage = *refImage;
+		if ( app.parameters["swap"].toString() == "image" || app.parameters["swap"].toString() == "both" )
+		{
 			switch (refImage->typeID()) {
 			case data::TypePtr<u_int8_t>::staticID:
-				newImage = voxelSwapZ<u_int8_t>( refImage );
+				newImage = voxelSwapZ<u_int8_t>( refImage, dim );
 				break;
 			case data::TypePtr<int8_t>::staticID:
-				newImage = voxelSwapZ<int8_t>( refImage );
+				newImage = voxelSwapZ<int8_t>( refImage, dim );
 				break;
 			case data::TypePtr<u_int16_t>::staticID:
-				newImage = voxelSwapZ<u_int16_t>( refImage );
+				newImage = voxelSwapZ<u_int16_t>( refImage, dim );
 				break;
 			case data::TypePtr<int16_t>::staticID:
-				newImage = voxelSwapZ<int16_t>( refImage );
+				newImage = voxelSwapZ<int16_t>( refImage, dim );
 				break;
 			case data::TypePtr<u_int32_t>::staticID:
-				newImage = voxelSwapZ<u_int32_t>( refImage );
+				newImage = voxelSwapZ<u_int32_t>( refImage, dim );
 				break;
 			case data::TypePtr<int32_t>::staticID:
-				newImage = voxelSwapZ<int32_t>( refImage );
+				newImage = voxelSwapZ<int32_t>( refImage, dim );
 				break;
 			case data::TypePtr<u_int64_t>::staticID:
-				newImage = voxelSwapZ<u_int64_t>( refImage );
+				newImage = voxelSwapZ<u_int64_t>( refImage, dim );
 				break;
 			case data::TypePtr<int64_t>::staticID:
-				newImage = voxelSwapZ<int64_t>( refImage );
+				newImage = voxelSwapZ<int64_t>( refImage, dim );
 				break;
 			case data::TypePtr<float>::staticID:
-				newImage = voxelSwapZ<float>( refImage );
+				newImage = voxelSwapZ<float>( refImage, dim );
 				break;
 			case data::TypePtr<double>::staticID:
-				newImage = voxelSwapZ<double>( refImage );
+				newImage = voxelSwapZ<double>( refImage, dim );
 				break;
 			default:
 				break;
 			}
-			finImageList.push_back(boost::shared_ptr<data::Image> ( new data::Image( newImage ) ) );
-		} else {
-			std::vector<boost::shared_ptr<data::Chunk> > chList = refImage->getChunkList();
-			BOOST_FOREACH( std::vector<boost::shared_ptr< data::Chunk> >::reference chRef, chList ) {
-				chRef->join( static_cast<util::PropMap>( *refImage ), false );
-				data::Chunk tmpChunk = chRef->cloneToMem( chRef->sizeToVector()[0], chRef->sizeToVector()[1], chRef->sizeToVector()[2], chRef->sizeToVector()[3] );
-
-				if ( app.parameters["swap"].toString() == "both" ) {
-					if ( !chRef->swapAlong( tmpChunk, dim, true ) ) {
-						LOG( data::Runtime, error) << "Swapping failed.";
-					}
-				} else if ( app.parameters["swap"].toString() == "image" ) {
-
-					if ( !chRef->swapAlong( tmpChunk, dim, false ) ) {
-						LOG( data::Runtime, error) << "Swapping failed.";
-					}
-				} else if ( app.parameters["swap"].toString() == "space" ) {
-					tmpChunk = *chRef;
-					boost::numeric::ublas::matrix<float> T( 3, 3 );
-					T( 0, 0 ) = 1;
-					T( 0, 1 ) = 0;
-					T( 0, 2 ) = 0;
-					T( 1, 0 ) = 0;
-					T( 1, 1 ) = 1;
-					T( 1, 2 ) = 0;
-					T( 2, 0 ) = 0;
-					T( 2, 1 ) = 0;
-					T( 2, 2 ) = 1;
-					T( dim, dim ) *= -1;
-					tmpChunk.transformCoords( T );
-				}
-
-				finChunkList.push_back( boost::shared_ptr<data::Chunk>( new data::Chunk( tmpChunk ) ) );
-			}
-			finImageList = data::ImageList(finChunkList);
 		}
+		if ( app.parameters["swap"].toString() == "both" || app.parameters["swap"].toString() == "space") {
+			newImage.transformCoords(T);
+			std::vector<boost::shared_ptr< data::Chunk> > chList = newImage.getChunkList();
+			BOOST_FOREACH(std::vector<boost::shared_ptr< data::Chunk> >::reference chRef, chList )
+			{
+				chRef->transformCoords(T);
+			}
+		}
+		finImageList.push_back(boost::shared_ptr<data::Image> ( new data::Image( newImage ) ) );
 	}
 	app.autowrite( finImageList );
 	return 0;
 };
-
-
 
 
 const size_t getBiggestVecElem( const util::fvector4& vec)
