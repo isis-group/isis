3546071767c29e5c2360cb3215c57f7939909d38
Erik Türke <tuerke@cbs.mpg.de>
2010-09-28 14:51:32 +0000

    added filling '0' to the index in the output filename

--- lib/DataStorage/io_interface.cpp
+++ lib/DataStorage/io_interface.cpp
@@ -1,5 +1,7 @@
 #include <boost/foreach.hpp>
 #include <boost/filesystem.hpp>
+#include <iomanip>
+#include <iostream>
 
 #include "CoreUtils/log.hpp"
 #include "ImageIO/common.hpp"
@@ -25,9 +27,12 @@ void FileFormat::write( const isis::data::ImageList &images, const std::string &
 		BOOST_FOREACH( data::ImageList::const_reference ref, images ) {
 			if ( ref->hasProperty( "sequenceNumber" ) ) {
 				sequenceSet.insert( ref->getProperty<u_int16_t>( "sequenceNumber" ) );
-				imagePerSequenceCounter.insert( std::pair<u_int16_t, size_t>( ref->getProperty<u_int16_t>( "sequenceNumber" ), 0 ) );
+				imagePerSequenceCounter.insert( std::pair<u_int16_t, unsigned short>( ref->getProperty<u_int16_t>( "sequenceNumber" ), 0 ) );
 			}
 		}
+		const unsigned short imageDigitsSequence = std::log10( sequenceSet.size() ) + 1;
+		const unsigned short imageDigitsImagePerSequenceCounter = std::log10( images.size() ) + 1;
+
 		if( sequenceSet.size() == images.size() ) {
 			uniqueSequenceNumber = true;
 		} else {
@@ -40,13 +45,17 @@ void FileFormat::write( const isis::data::ImageList &images, const std::string &
 				ret = false;
 				continue;
 			}
+			std::stringstream sequenceNumber;
+			std::stringstream sequenceCounter;
+			sequenceCounter << std::setfill('0') << std::setw( imageDigitsImagePerSequenceCounter ) << boost::lexical_cast<std::string>(  ++imagePerSequenceCounter[ref->getProperty<u_int16_t>( "sequenceNumber" ) ] );
+			sequenceNumber << std::setfill('0') << std::setw( imageDigitsSequence ) << ref->getProperty<std::string>( "sequenceNumber" );
 			std::string unique_name = "";
 			if ( uniqueSequenceNumber ) {
-				unique_name = std::string( "S" ) + ref->getProperty<std::string>( "sequenceNumber" ) + "_" + file;
+				unique_name = std::string( "S" ) + sequenceNumber.str() + "_" + file;
 			}
 			//if we have more than one image with the same sequenceNumber we have to add a second index to the filename to avoid overwriting
 			else {
-				unique_name = std::string( "S" ) + ref->getProperty<std::string>( "sequenceNumber" ) + "_" + boost::lexical_cast<std::string>( ++imagePerSequenceCounter[ref->getProperty<u_int16_t>( "sequenceNumber" )] ) + "_" + file;
+				unique_name = std::string( "S" ) + sequenceNumber.str() + "_" + sequenceCounter.str()  + "_" + file;
 			}
 			LOG( Runtime, info )   << "Writing image to " <<  path / unique_name;
 			write( *ref, ( path / unique_name ).string(), dialect );
