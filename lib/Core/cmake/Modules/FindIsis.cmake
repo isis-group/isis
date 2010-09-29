4cff7659dfa281198707e6d16e488d703308328a
Thomas Pröger <proeger@cbs.mpg.de>
2010-09-29 09:25:19 +0000

    - added install targets to the core library

--- lib/Core/cmake/Modules/FindIsis.cmake
+++ lib/Core/cmake/Modules/FindIsis.cmake
@@ -1,8 +1,15 @@
 # FindISIS
 # 
-# Searchs the ISIS core library libsisis.so and the corresponding header files.
+# This script search the ISIS core library libsisis.so and the corresponding header files.
+# It also looks for the ISIS build configuration file.
+#
+# Supports ISIS Version 0.2.0
 #
 
-
-
+# Provided variables:
+#
+# ISIS_FOUND             TRUE if all ISIS libraries were found, FALSE otherwise.
+# ISIS_INCLUDE_DIR       the path to the header directory
+# ISIS_LIBRARY_DIR       the path to the libisis.so and libisis.a library file.
+#
 
