3ca7e5124c9acdf33dcc75c6a408e03c45ada6b5
Enrico Reimer <reimer@cbs.mpg.de>
2010-11-04 14:19:04 +0100

    added test for FileFormat::makeUniqueFilenames

--- tests/ImageIO/imageIOTest.cpp
+++ tests/ImageIO/imageIOTest.cpp
@@ -39,6 +39,30 @@ BOOST_AUTO_TEST_CASE ( imageNameGenTest )
 	);
 }
 
+BOOST_AUTO_TEST_CASE ( imageUniqueName )
+{
+	data::MemChunk<uint8_t> ch1(5,5,5);
+	data::ChunkList chunks;
+	data::Image img;
+	ch1.setProperty( "indexOrigin", util::fvector4( 0, 0, 2 ) );
+	ch1.setProperty<uint32_t>( "acquisitionNumber", 0 );
+	ch1.setProperty<float>( "acquisitionTime", 0 );
+	ch1.setProperty( "readVec", util::fvector4( 1, 0 ) );
+	ch1.setProperty( "phaseVec", util::fvector4( 0, 1 ) );
+	ch1.setProperty( "voxelSize", util::fvector4( 1, 1, 1, 0 ) );
+
+	for(uint32_t i=0;i<5;i++){ // make some copies of ch1 - change their acquisitionNumber and put the into the list
+		boost::shared_ptr<data::Chunk> ch(new data::MemChunk<uint8_t>(ch1));
+		ch->setProperty<uint32_t>( "acquisitionNumber", i );
+		chunks.push_back(ch);
+	}
+	data::ImageList images(chunks);
+
+	BOOST_REQUIRE_EQUAL(images.size(),5);int number=0;
+	BOOST_FOREACH(const std::string &ref,image_io::FileFormat::makeUniqueFilenames(images,"/tmp/S{acquisitionNumber}.nii")){
+		BOOST_REQUIRE_EQUAL(ref,std::string("/tmp/S.nii").insert(6,util::Type<uint32_t>(number++).toString(false)));
+	}
+}
 
 }
 }
