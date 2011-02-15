ba977f31f26cdfad5a8ecde332f514a0364f595f
Enrico Reimer <reimer@cbs.mpg.de>
2011-02-15 21:06:04 +0100

    add simple input for uint16bit data

--- lib/ImageIO/imageFormat_raw.cpp
+++ lib/ImageIO/imageFormat_raw.cpp
@@ -1,6 +1,9 @@
 #include <DataStorage/io_interface.h>
 #include <fstream>
 #include <boost/foreach.hpp>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <boost/filesystem.hpp>
 
 namespace isis
 {
@@ -13,6 +16,26 @@ protected:
 	std::string suffixes()const {
 		return std::string( "raw" );
 	}
+	struct Deleter {
+		//members
+		int m_file;
+		std::string m_filename;
+		Deleter( int file, const std::string &filename ):m_file(file),m_filename(filename){}
+
+		void operator ()( void *at ) {
+			LOG(Debug,info) << "Freeing memory at " << at << " mapped from " << m_filename;
+			munmap( at, boost::filesystem::file_size(m_filename) );//@todo will fail when the file was replaced in the meantime
+			close( m_file );
+		}
+	};
+
+	class RawMemChunk:public data::Chunk
+	{
+	public:
+		template<typename T> RawMemChunk(T *src,const Deleter &del, size_t width, size_t height, size_t slices=1, size_t timesteps=1)
+		:data::Chunk( src, del, width, height, slices, timesteps ){}
+	};
+
 public:
 	std::string name()const {
 		return "raw data output";
@@ -20,8 +43,42 @@ public:
 
 
 	int load ( std::list<data::Chunk> &chunks, const std::string &filename, const std::string &dialect )  throw( std::runtime_error & ) {
-		throwGenericError( "Sorry raw input is not supported yet" );
-		return 0;
+
+
+		const size_t fsize=boost::filesystem::file_size(filename);
+		const size_t ssize=sqrt(fsize/sizeof(uint16_t));
+
+		if(ssize*ssize*sizeof(uint16_t) == fsize){
+			LOG(Runtime,info) << "Guessing size of read and phase to be " << ssize;
+
+			int mfile = open( filename.c_str(), O_RDONLY);
+			if( mfile == -1 ) {
+				throwSystemError( errno, std::string( "Failed to open " ) + filename );
+			}
+
+
+			char *mmem = ( char * )mmap( NULL, fsize, PROT_READ, MAP_SHARED, mfile, 0 ); //map it into memory
+			if( mmem == MAP_FAILED ) {
+				throwSystemError( errno, std::string( "Failed to map " ) + filename + " into memory" );
+			}
+	// 		std::map<unsigned short,std::string>::const_iterator found=util::getTypeMap().find(dialect);
+	// 		if(found==util::getTypeMap().end()){
+	// 			throwGenericError(std::string("Datatype \"")+dialect+"\" not found");
+	// 		}
+
+			Deleter del(mfile,filename);
+			chunks.push_back(RawMemChunk((uint16_t*)mmem,del,ssize,ssize));
+			data::Chunk &ch=chunks.back();
+			ch.setPropertyAs<uint16_t>("sequenceNumber",0);
+			ch.setPropertyAs<uint32_t>("acquisitionNumber",0);
+			ch.setPropertyAs("voxelSize",util::fvector4(1,1,1));
+			ch.setPropertyAs("readVec",util::fvector4(1,0));
+			ch.setPropertyAs("phaseVec",util::fvector4(0,1));
+			ch.setPropertyAs("indexOrigin",util::fvector4(0,0));
+			return 1;
+		} else {
+			LOG(Runtime,error) << "Could not guess image size for " << fsize << " bytes of data";
+		}
 	}
 
 	void write( const data::Image &image, const std::string &filename, const std::string &dialect )  throw( std::runtime_error & ) {
