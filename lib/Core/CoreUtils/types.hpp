bbf6264cccd5fc8d0180c157154d619cd43cbb3a
Enrico Reimer <reimer@cbs.mpg.de>
2011-02-17 15:17:56 +0100

    fix typo (witValues)

--- lib/Core/CoreUtils/types.hpp
+++ lib/Core/CoreUtils/types.hpp
@@ -70,9 +70,9 @@ template< typename T > struct checkType {
 	);
 };
 
-std::map<unsigned short, std::string> getTypeMap( bool witValues = true, bool withValuePtrs = true );
+std::map<unsigned short, std::string> getTypeMap( bool withValues = true, bool withValuePtrs = true );
 
-std::map< std::string, unsigned short> getTransposedTypeMap( bool witValues = true, bool withValuePtrs = true );
+std::map< std::string, unsigned short> getTransposedTypeMap( bool withValues = true, bool withValuePtrs = true );
 }
 }
 
