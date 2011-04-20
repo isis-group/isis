ea6d9e13ac34fbf54474bade2e997d086859b50b
Erik Tuerke <tuerke@cbs.mpg.de>
2011-04-20 13:50:30 +0200

    if we now load more than one image with the same image name isisViewer creates a new name with an increasing number

--- apps/isisViewer/viewer/DataContainer.cpp
+++ apps/isisViewer/viewer/DataContainer.cpp
@@ -7,10 +7,24 @@ namespace viewer
 
 bool DataContainer::addImage( const data::Image &image, const ImageHolder::ImageType &imageType, const std::string &filename )
 {
+	std::string newFileName = filename;
+	if(find(filename) != end() ) {
+		size_t index = 0;
+		std::cout << "gna" << std::endl;
+		while (find(newFileName) != end() ) {
+			std::stringstream ss;
+			ss << filename << " (" << ++index << ")";
+			newFileName = ss.str();
+			std::cout << newFileName << std::endl;
+		}
+	}
 	ImageHolder tmpHolder;
-	tmpHolder.setImage( image, imageType, filename );
+	tmpHolder.setImage( image, imageType, newFileName );
 	tmpHolder.setID( size() );
-	insert( std::make_pair<std::string, ImageHolder>( filename, tmpHolder ) );
+	//we have to check if the filename already exists and if so adding 
+		
+	
+	insert( std::make_pair<std::string, ImageHolder>( newFileName, tmpHolder ) );
 
 	return true;
 }
