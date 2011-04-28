d342845d43c606b58897cbf569c86d23fff438e7
Enrico Reimer <reimer@cbs.mpg.de>
2011-04-28 20:07:24 +0800

    add NDimensional::getCoordsFromLinIndex

--- lib/Core/DataStorage/ndimensional.hpp
+++ lib/Core/DataStorage/ndimensional.hpp
@@ -39,6 +39,12 @@ template<unsigned short DIM> size_t __dim2index( const size_t d[], const size_t 
 	return d[DIM] * __dimStride<DIM>( dim ) + __dim2index < DIM - 1 > ( d, dim );
 }
 
+template<unsigned short DIM> void __index2dim(const size_t index, size_t d[], const size_t dim[],size_t vol )
+{
+	d[DIM]=index/vol;
+	__index2dim<DIM-1>(index%vol,d,dim,vol/dim[DIM-1]);
+}
+
 template<unsigned short DIM> bool __rangeCheck( const size_t d[], const size_t dim[] )
 {
 	return ( d[DIM] < dim[DIM] ) && __rangeCheck < DIM - 1 > ( d, dim );
@@ -46,6 +52,7 @@ template<unsigned short DIM> bool __rangeCheck( const size_t d[], const size_t d
 
 template<> inline size_t __dimStride<0>( const size_t[] /*dim*/ ) {return 1;}
 template<> inline size_t __dim2index<0>( const size_t d[], const size_t dim[] ) {return d[0] * __dimStride<0>( dim );}
+template<> inline void __index2dim<0>( const size_t index, size_t d[], const size_t[],size_t vol ) {d[0]=index;}
 template<> inline bool   __rangeCheck<0>( const size_t d[], const size_t dim[] ) {return d[0] < dim[0];}
 
 /// @endcond
@@ -80,6 +87,13 @@ public:
 	 */
 	size_t getLinearIndex( const size_t d[DIMS] )const {
 		return __dim2index < DIMS - 1 > ( d, m_dim );
+	}
+	/**
+	 * Compute linear index from n-dimensional index,
+	 * \param d array of indexes (d[0] is most iterating element / lowest dimension)
+	 */
+	void getCoordsFromLinIndex(const size_t index,size_t d[DIMS] )const {
+		__index2dim < DIMS - 1 > (index, d, m_dim,getVolume()/m_dim[DIMS - 1] );
 	}
 	/**
 	 * Check if index fits into the dimensional size of the object.
