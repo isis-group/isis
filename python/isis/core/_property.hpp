26e89628920cfc2bba73b844be75e72a7d11fbed
Erik Tuerke <tuerke@cbs.mpg.de>
2011-09-16 14:54:47 +0200

    fixed installation of python libs

--- python/isis/core/_property.hpp
+++ python/isis/core/_property.hpp
@@ -8,6 +8,8 @@
 #ifndef _PROPERTY_HPP_
 #define _PROPERTY_HPP_
 
+#include "CoreUtils/property.hpp"
+
 using namespace isis::util;
 
 namespace isis
@@ -16,13 +18,13 @@ namespace python
 {
 
 
-class _TypeValue : public PropertyValue, boost::python::wrapper< PropertyValue >
+class _PropertyValue : public PropertyValue, boost::python::wrapper< PropertyValue >
 {
 
 public:
-	_TypeValue () {}
-	_TypeValue ( PyObject *p ) : boost::python::wrapper< PropertyValue >(),  self( p ) {}
-	_TypeValue ( PyObject *p, const PropertyValue &base ) : PropertyValue( base ),  boost::python::wrapper< PropertyValue >(), self( p ) {}
+	_PropertyValue () {}
+	_PropertyValue ( PyObject *p ) : boost::python::wrapper< PropertyValue >(),  self( p ) {}
+	_PropertyValue ( PyObject *p, const PropertyValue &base ) : PropertyValue( base ),  boost::python::wrapper< PropertyValue >(), self( p ) {}
 
 	std::string _toString( bool label ) { return this->toString( label ); }
 private:
