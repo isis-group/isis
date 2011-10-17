29b85939dc479a1a7e39d9bbc71de4fec30599c0
Erik Tuerke <tuerke@cbs.mpg.de>
2011-10-17 17:15:07 +0200

    add respective typeid to each type enum

--- python/isis/core/common.hpp
+++ python/isis/core/common.hpp
@@ -10,6 +10,7 @@
 
 #include "CoreUtils/log.hpp"
 #include "CoreUtils/common.hpp"
+#include "CoreUtils/type.hpp"
 
 /*! \addtogroup python
 *  Additional documentation for group `mygrp'
@@ -35,9 +36,30 @@ namespace core
 {
 namespace _internal
 {
-enum types { BOOL, INT8_T, UINT8_T, INT16_T, UINT16_T, INT32_T, UINT32_T, INT64_T, UINT64_T, FLOAT, DOUBLE,
-FVECTOR4, DVECTOR4, IVECTOR4, ILIST, DLIST, SLIST, STDSTRING, SELECTION, COMPLEX_FLOAT,
-COMPLEX_DOUBLE, BOOST_PTIME, BOOST_DATE };
+using namespace isis::util;
+enum types { BOOL = Value<bool>::staticID, 
+INT8_T = Value<int_fast8_t>::staticID, 
+UINT8_T = Value<uint8_t>::staticID, 
+INT16_T = Value<int16_t>::staticID, 
+UINT16_T = Value<uint16_t>::staticID, 
+INT32_T = Value<int32_t>::staticID, 
+UINT32_T = Value<uint32_t>::staticID, 
+INT64_T = Value<int64_t>::staticID, 
+UINT64_T = Value<uint64_t>::staticID, 
+FLOAT = Value<float>::staticID, 
+DOUBLE = Value<double>::staticID,
+FVECTOR4 = Value<fvector4>::staticID, 
+DVECTOR4 = Value<dvector4>::staticID, 
+IVECTOR4 = Value<ivector4>::staticID, 
+ILIST = Value<ilist>::staticID, 
+DLIST = Value<dlist>::staticID, 
+SLIST = Value<slist>::staticID, 
+STDSTRING = Value<std::string>::staticID, 
+SELECTION = Value<Selection>::staticID, 
+COMPLEX_FLOAT = Value< std::complex<float> >::staticID,
+COMPLEX_DOUBLE = Value< std::complex<double> >::staticID, 
+BOOST_PTIME = Value<boost::posix_time::ptime>::staticID, 
+BOOST_DATE = Value<boost::gregorian::date>::staticID };
 } // namespace _internal
 
 } // namespace core
--- python/isis/data/common.hpp
+++ python/isis/data/common.hpp
@@ -36,7 +36,18 @@ template<typename HANDLE> void enable_log( LogLevel level )
 }
 namespace data {
 namespace _internal {
-enum image_types { BOOL = 1, INT8_T, UINT8_T, INT16_T, UINT16_T, INT32_T, UINT32_T, INT64_T, UINT64_T, FLOAT, DOUBLE };
+using namespace isis::data;
+enum image_types { BOOL = ValuePtr<bool>::staticID,
+INT8_T = ValuePtr<int8_t>::staticID, 
+UINT8_T = ValuePtr<uint8_t>::staticID, 
+INT16_T = ValuePtr<int16_t>::staticID, 
+UINT16_T = ValuePtr<uint16_t>::staticID, 
+INT32_T = ValuePtr<int32_t>::staticID, 
+UINT32_T = ValuePtr<uint32_t>::staticID, 
+INT64_T = ValuePtr<int64_t>::staticID, 
+UINT64_T = ValuePtr<uint64_t>::staticID, 
+FLOAT = ValuePtr<float>::staticID, 
+DOUBLE = ValuePtr<double>::staticID };
 
 
 using namespace isis::data;
