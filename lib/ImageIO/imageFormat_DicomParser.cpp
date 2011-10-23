49fc93de9aad973c704f75917221d640eb01069b
Enrico Reimer <reimer@cbs.mpg.de>
2011-10-23 23:39:30 +0200

    switch du usage of FindDCMTK

--- lib/ImageIO/imageFormat_DicomParser.cpp
+++ lib/ImageIO/imageFormat_DicomParser.cpp
@@ -2,8 +2,8 @@
 #include <boost/date_time/gregorian/gregorian.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <DataStorage/common.hpp>
-#include "dcmtk/dcmdata/dcdict.h"
-#include "dcmtk/dcmdata/dcdicent.h"
+#include <dcdict.h>
+#include <dcdicent.h>
 
 namespace isis
 {
