ee455e593fdcc0c0bb02db207e1e7b9f0a4bd278
Enrico Reimer <reimer@cbs.mpg.de>
2012-03-06 19:30:22 +0100

    improve isisdiff to better read/diff multiple images from different sources

--- tools/isisdiff.cpp
+++ tools/isisdiff.cpp
@@ -5,6 +5,10 @@
 #include <boost/lexical_cast.hpp>
 #include <boost/bind.hpp>
 
+
+struct DiffLog {static const char *name() {return "Diff";}; enum {use = _ENABLE_LOG};};
+struct DiffDebug {static const char *name() {return "DiffDebug";}; enum {use = _ENABLE_DEBUG};};
+
 using namespace isis;
 
 void dropDuplicate( std::list<data::Image> &list )
@@ -17,14 +21,54 @@ void dropDuplicate( std::list<data::Image> &list )
 			const util::PropertyMap &aref = *a, &bref = *b;
 
 			if( aref.getDifference( bref ).empty() ) {
-				std::cout << "Duplicate found in data from "
-						  << bref.propertyValue( "source" ).toString( false ) << ":" << std::distance( list.begin(), b )
-						  << ", dropping.." << std::endl;
+				LOG( DiffLog, notice ) << "Duplicate found in data from "
+									   << bref.propertyValue( "source" ).toString( false ) << ":" << std::distance( list.begin(), b )
+									   << ", dropping..";
 				list.erase( b++ );
 			} else
 				++b;
 		}
 	}
+}
+
+boost::filesystem::path getCommonSource( std::list<boost::filesystem::path> sources )
+{
+	sources.erase( std::unique( sources.begin(), sources.end() ), sources.end() );
+
+	if( sources.empty() ) {
+		LOG( DiffLog, error ) << "Failed to get common source";
+		return boost::filesystem::path();
+	} else if( sources.size() == 1 )
+		return sources.front();
+	else {
+		BOOST_FOREACH( boost::filesystem::path & ref, sources )
+		ref.remove_filename();
+		return getCommonSource( sources );
+	}
+}
+
+boost::filesystem::path getCommonSource( const data::Image img )
+{
+	if( img.hasProperty( "source" ) )
+		return img.getPropertyAs<std::string>( "source" );
+	else {
+		std::list<boost::filesystem::path> sources;
+		BOOST_FOREACH( const util::PropertyValue & ref, img.getChunksProperties( "source", true ) ) {
+			sources.push_back( ref->as<std::string>() );
+		}
+		return getCommonSource( sources );
+	}
+}
+
+std::string identify( const data::Image &img )
+{
+	return
+		getCommonSource( img ).file_string()
+		+ "[S"
+		+ img.getPropertyAs<std::string>( "sequenceNumber" )
+		+ ( img.hasProperty( "sequenceDescription" ) ? ( "_" +     img.getPropertyAs<std::string>( "sequenceDescription" ) ) : "" )
+		+ ( img.hasProperty( "sequenceStart" )       ? ( " from " + img.getPropertyAs<std::string>( "sequenceStart" ) )      : "" )
+		+ "]";
 }
 
 std::pair<std::string, int>  parseFilename( std::string name )
@@ -47,30 +91,76 @@ bool hasDifferentProp( const data::Image &img, const util::PropertyMap::PropPath
 {
 	return !( img.propertyValue( pname ) == pval ); // if property does not exist, an empty propertyValue is returned and those compare unequal to everything
 }
+bool hasSameProp( const data::Image &img, const util::PropertyMap::PropPath &pname, util::PropertyValue pval )
+{
+	return img.propertyValue( pname ) == pval; // if property does not exist, an empty propertyValue is returned and those compare unequal to everything
+}
 
-std::list<data::Image> pickImg( int pos, std::list<data::Image> list )
+data::Image pickImg( int pos, std::list<data::Image> list )
 {
 	std::list< data::Image >::iterator at = list.begin();
 	std::advance( at, pos );
-	return std::list<data::Image>( 1, *at );
+	return *at;
 }
 
-std::list<data::Image> findFitting( const data::Image reference, std::list<data::Image> images )
+bool doFit( const data::Image reference, std::list<data::Image> &org_images, std::list<data::Image> &images, const char *propName )
 {
-	images.remove_if( boost::bind( hasDifferentProp, _1, util::PropertyMap::PropPath( "sequenceNumber" ), reference.propertyValue( "sequenceNumber" ) ) );
+	const util::PropertyMap::PropPath propPath( propName );
+	images.remove_if( boost::bind( hasDifferentProp, _1, propPath , reference.propertyValue( propPath ) ) );
+	org_images.remove_if( boost::bind( hasSameProp, _1, propPath, reference.propertyValue( propPath ) ) );
 
-	if( images.size() <= 1 )return images;
+	if( images.size() <= 1 ) {
+		LOG( DiffDebug, info ) << "Found a unique image with the same " << propName << " " << util::MSubject( reference.propertyValue( propPath ) ) << ", returning....";
+		return true;
+	} else
+		return false;
+}
 
-	images.remove_if( boost::bind( hasDifferentProp, _1, util::PropertyMap::PropPath( "sequenceDescription" ), reference.propertyValue( "sequenceDescription" ) ) );
+std::list<data::Image> findFitting( const data::Image reference, std::list<data::Image> &org_images )
+{
+	std::list< data::Image > images = org_images;
+	const char *props[] = {"sequenceNumber", "sequenceDescription", "sequenceStart"};
 
-	if( images.size() <= 1 )return images;
-
-	images.remove_if( boost::bind( hasDifferentProp, _1, util::PropertyMap::PropPath( "sequenceStart" ), reference.propertyValue( "sequenceStart" ) ) );
-
-	if( images.size() > 1 )
-		std::cout << "still more than one image" << std::endl;;
-
+	BOOST_FOREACH( const char * prop, props ) {
+		if( doFit( reference, org_images, images, prop ) )
+			return images;
+	}
 	return images;
+}
+
+bool diff( const data::Image &img1, const data::Image &img2, const util::slist &ignore )
+{
+	bool ret = false;
+	util::PropertyMap::DiffMap diff = img1.getDifference( img2 );
+	BOOST_FOREACH( util::slist::const_reference ref, ignore ) {
+		diff.erase( util::istring( ref.begin(), ref.end() ) );
+	}
+	const std::string name1 = identify( img1 );
+	const std::string name2 = identify( img2 );
+
+	if ( ! diff.empty() ) {
+		std::cout
+				<< "Metadata of " << name1 << " and " << name2  << " differ:" << std::endl
+				<< diff << std::endl;
+		ret = true;
+	}
+
+	if ( img1.getSizeAsVector() != img2.getSizeAsVector() ) {
+		std::cout
+				<< "Image sizes of " << name1 << " and " << name2  << " differ:"
+				<< img1.getSizeAsString() << "/" << img2.getSizeAsString() << std::endl;
+		ret = true;
+	} else {
+		size_t voxels = img1.compare( img2 );
+
+		if ( voxels != 0 ) {
+			std::cout << voxels * 100 / img1.getVolume() << "% of the voxels in " << name1 << " and "
+					  << name2  << " differ" << std::endl;
+			ret = true;
+		}
+	}
+
+	return ret;
 }
 
 int main( int argc, char *argv[] )
@@ -80,6 +170,8 @@ int main( int argc, char *argv[] )
 	app.parameters["ignore"].needed() = false;
 	app.parameters["ignore"].setDescription( "List of properties which should be ignored when comparing" );
 
+	app.addLogging<DiffLog>( "Diff" );
+	app.addLogging<DiffDebug>( "Diff" );
 
 	data::IOApplication::addInput( app.parameters, true, "1", " of the first image" );
 	data::IOApplication::addInput( app.parameters, true, "2", " of the second image" );
@@ -98,81 +190,73 @@ int main( int argc, char *argv[] )
 	else
 		in2.second = -1;
 
+	size_t ret = 0;
 	std::list<data::Image> images1, images2;
+	util::slist ignore = app.parameters["ignore"];
+	ignore.push_back( "source" );
+	boost::shared_ptr<util::ConsoleFeedback> feedback( new util::ConsoleFeedback );
 
 	if( in1.second >= 0 && in2.second >= 0 ) { // seems like we got numbers
-		std::cout << "Comparing image " << in1.second << " from " << in1.first << " and Image " << in2.second  << " from " << in2.first << std::endl;
-		app.parameters["in1"] = in1.first;
+		LOG( DiffLog, notice ) << "Comparing image " << in1.second << " from " << in1.first << " and Image " << in2.second  << " from " << in2.first;
+		app.parameters["in1"] = util::slist( 1, in1.first );
 		std::list<data::Image> images;
-		data::IOApplication::autoload( app.parameters, images, true, "1" );
+		data::IOApplication::autoload( app.parameters, images, true, "1", feedback );
 
-		images1 = pickImg( in1.second, images );
+		const data::Image first = pickImg( in1.second, images );
 
 		if( !in2.first.empty() ) {
 			images.clear();
-			data::IOApplication::autoload( app.parameters, images, true, "2" );
+			app.parameters["in2"] = util::slist( 1, in2.first );
+			data::IOApplication::autoload( app.parameters, images, true, "2", feedback );
 		}
 
-		images2 = pickImg( in2.second, images );
+		const data::Image second = pickImg( in2.second, images );
+
+		if( diff( first, second, ignore ) )
+			ret = 1;
+
 	} else if( in1.second <= 0 && in2.second <= 0 ) {
-		data::IOApplication::autoload( app.parameters, images1, true, "1" );
-		data::IOApplication::autoload( app.parameters, images2, true, "2" );
+		LOG( DiffLog, info ) << "Comparing whole image sets from \"" << in1.first << "\" and from \"" << in2.first << "\"";
+		data::IOApplication::autoload( app.parameters, images1, true, "1", feedback );
+		data::IOApplication::autoload( app.parameters, images2, true, "2", feedback );
 		dropDuplicate( images1 );
 		dropDuplicate( images2 );
 
-		if ( images1.size() != images2.size() ) {
-			std::cout << "Amount of found images in " << in1.first << "(" << images1.size() << ") and " << in1.first << "(" << images2.size() << ") differs" << std::endl;
-			return -1;
+		for (
+			std::list< data::Image >::iterator first = images1.begin();
+			first != images1.end() && !images1.empty();
+		) {
+			const std::list<data::Image> candidates = findFitting( *first, images2 );
+			LOG_IF( candidates.size() > 1 || candidates.empty(), DiffLog, warning )
+					<< "Could not find a unique image fitting " << identify( *first );
+
+			BOOST_FOREACH( const data::Image & second, candidates ) {
+				if( diff( *first, second, ignore ) )
+					ret++;
+			}
+
+			if( !candidates.empty() ) { // if we did a comparison - remove the image from the compare list
+				images1.erase( first++ );
+			} else
+				++first; // of not skip that one, and go on
+		}
+
+		if( !images1.empty() ) {
+			std::cout << "there are " << images1.size() << " images left from " << in1.first << std::endl;
+			ret += images1.size();
+		}
+
+		if( !images2.empty() ) {
+			std::cout << "there are " << images2.size() << " images left from " << in2.first << std::endl;
+			ret += images2.size();
 		}
 	} else {
-		std::cerr << "Cannot mix single and multi image mode" << std::cerr;
+		LOG( DiffLog, error ) << "Cannot mix single and multi image mode, exiting.";
+		return -1;
 	}
 
 
 
-	int count;
-	util::slist ignore = app.parameters["ignore"];
-	ignore.push_back( "source" );
-	size_t ret = 0;
-
-	for ( std::list<data::Image>::const_iterator i = images1.begin(); i != images1.end(); i++, count++ ) {
-		const data::Image &first = *i;
-
-		const std::list<data::Image> candidates = findFitting( first, images2 );
-
-		const data::Image &second = candidates.front();
-
-		util::PropertyMap::DiffMap diff = first.getDifference( second );
-		BOOST_FOREACH( util::slist::const_reference ref, ignore ) {
-			diff.erase( util::istring( ref.begin(), ref.end() ) );
-		}
-		ret += diff.size();
-		const std::string countStr = ( images1.size() > 1 ? std::string( ":" ) + boost::lexical_cast<std::string>( count ) : std::string( "" ) );
-
-		if ( ! diff.empty() ) {
-			std::cout
-					<< "Metadata of " << app.parameters["in1"]->as<std::string>() << countStr  << " and "
-					<< app.parameters["in2"]->as<std::string>() << countStr  << " differ:" << std::endl
-					<< diff << std::endl;
-		}
-
-		if ( first.getSizeAsVector() != second.getSizeAsVector() ) {
-			std::cout
-					<< "Image sizes of " << app.parameters["in1"]->as<std::string>() << countStr << " and "
-					<< app.parameters["in2"]->as<std::string>() << countStr  << " differ:"
-					<< first.getSizeAsString() << "/" << second.getSizeAsString() << std::endl;
-			ret++;
-		} else {
-			size_t voxels = first.compare( second );
-
-			if ( voxels != 0 ) {
-				std::cout << voxels * 100 / first.getSizeAsVector().product()
-						  << "% of the voxels in " << app.parameters["in1"]->as<std::string>() << countStr << " and "
-						  << app.parameters["in2"]->as<std::string>() << countStr  << " differ" << std::endl;
-				ret++;
-			}
-		}
-	}
 
 	return ret;
 }
