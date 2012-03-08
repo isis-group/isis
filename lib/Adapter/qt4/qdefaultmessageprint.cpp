1a767d1e71c499f8a896d7d9c8e9a00aadc88858
Erik Tuerke <tuerke@cbs.mpg.de>
2012-03-08 14:04:23 +0100

    add capability to show QMEssageBox in QDefaultMessagePrint module

--- lib/Adapter/qt4/qdefaultmessageprint.cpp
+++ lib/Adapter/qt4/qdefaultmessageprint.cpp
@@ -1,7 +1,11 @@
 #include "qdefaultmessageprint.hpp"
 #include <CoreUtils/singletons.hpp>
+#include <QMessageBox>
 
-isis::qt4::QDefaultMessagePrint::QDefaultMessagePrint( isis::LogLevel level ): MessageHandlerBase( level ) {}
+isis::qt4::QDefaultMessagePrint::QDefaultMessagePrint( isis::LogLevel level )
+	: MessageHandlerBase( level ),
+	m_QMessageLogLevel( isis::error )
+{}
 
 
 void isis::qt4::QDefaultMessagePrint::commit( const isis::util::_internal::Message &msg )
@@ -18,6 +22,26 @@ void isis::qt4::QDefaultMessagePrint::commit( const isis::util::_internal::Messa
 	qMessage.time_str = msg.strTime();
 	util::Singletons::get<QMessageList, 10>().push_back( qMessage );
 	commitMessage( qMessage );
+	if( m_QMessageLogLevel < msg.m_level ) {
+		QMessageBox msgBox;
+		switch( msg.m_level ) {
+			case isis::info:
+			case isis::verbose_info:
+			case isis::notice:
+				msgBox.setIcon(QMessageBox::Information);
+				break;
+			case isis::warning:
+				msgBox.setIcon( QMessageBox::Warning );
+				break;
+			case isis::error:
+				msgBox.setIcon( QMessageBox::Critical );
+				break;
+		}
+		std::stringstream errorMessage;
+		errorMessage << qMessage.m_module << "(" << qMessage.time_str << ") [" << qMessage.m_file << ":" << qMessage.m_line << "] " << qMessage.message;
+		msgBox.setText( errorMessage.str().c_str() );
+		msgBox.exec();
+	}
 }
 
 isis::qt4::QDefaultMessagePrint::~QDefaultMessagePrint()
