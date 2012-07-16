7fd9c813b2fba8978c86ea0117440f221fbf5915
Enrico Reimer <reimer@cbs.mpg.de>
2012-07-16 15:22:47 +0200

    adapt basic filter test to vector3

--- tests/Filter/basicFilterTest.cpp
+++ tests/Filter/basicFilterTest.cpp
@@ -15,10 +15,10 @@ namespace test
 data::Image createEmptyImage( util::ivector4 size )
 {
 	data::MemChunk<uint8_t> myChunk ( size[0], size[1], size[2], size[3] ) ;
-	myChunk.setPropertyAs<util::fvector4>( "indexOrigin", util::fvector3() );
-	myChunk.setPropertyAs<util::fvector4>( "rowVec", util::fvector4( 1, 0, 0, 0 ) );
-	myChunk.setPropertyAs<util::fvector4>( "columnVec", util::fvector4( 0, 1, 0, 0 ) );
-	myChunk.setPropertyAs<util::fvector4>( "voxelSize", util::fvector4( 1, 1, 1, 1 ) );
+	myChunk.setPropertyAs<util::fvector3>( "indexOrigin", util::fvector3() );
+	myChunk.setPropertyAs<util::fvector3>( "rowVec", util::fvector3( 1, 0, 0 ) );
+	myChunk.setPropertyAs<util::fvector3>( "columnVec", util::fvector3( 0, 1, 0 ) );
+	myChunk.setPropertyAs<util::fvector3>( "voxelSize", util::fvector3( 1, 1, 1 ) );
 	myChunk.setPropertyAs<uint16_t>( "acquisitionNumber", 0 );
 	return data::Image( myChunk );
 }
