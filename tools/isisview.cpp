2d60579fd8cb3e47d2160b9200d269ad865694be
Enrico Reimer <reimer@cbs.mpg.de>
2016-09-09 18:38:55 +0200

    more cosmetics

--- tools/isisview.cpp
+++ tools/isisview.cpp
@@ -3,14 +3,16 @@
 #include <isis/adapter/qt5/qtapplication.hpp>
 #include <isis/adapter/qt5/simpleimageview.hpp>
 
+using namespace isis::qt5;
+
 int main(int argc, char **argv) {
 
-	isis::qt5::IOQtApplication app("isisview",true,false);
-// 	app.getLogHandler(isis::data::Debug::name(),isis::warning)->stopBelow(isis::notice);
+	IOQtApplication app("isisview",true,false);
+// 	app.getLogHandler(data::Debug::name(),warning)->stopBelow(notice);
 	app.init(argc,argv);
 
 	while(!app.images.empty()){
-		isis::qt5::display(app.fetchImage());
+		display(app.fetchImage());
 	}
     return app.exec();
 }
