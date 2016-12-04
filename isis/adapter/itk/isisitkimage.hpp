4984a144b9746cec0606bc88e1858e5e2b4f65b7
Enrico Reimer <reimer@cbs.mpg.de>
2016-12-04 22:47:49 +0100

    update isistransform (can do resampling now)

--- isis/adapter/itk/isisitkimage.hpp
+++ isis/adapter/itk/isisitkimage.hpp
@@ -22,30 +22,77 @@
 
 #include <itkImage.h>
 #include "../../data/image.hpp"
+#include "../../data/chunk.hpp"
+#include <itkImportImageContainer.h>
+#include "common.hpp"
+
+
 
 namespace isis{
 namespace itk4{
 namespace _internal{
-	template<typename TYPE> class ContainerAdapter:public itk::Image<TYPE,4>::PixelContainer{
-	public:
-		typedef ContainerAdapter<TYPE>                      Self;
-		typedef typename itk::Image<TYPE,4>::PixelContainer Superclass;
-		typedef itk::SmartPointer< Self >                   Pointer;
-		typedef itk::SmartPointer< const Self >             ConstPointer;
+	
 
-		/** Method for creation through the object factory. */
-		itkNewMacro(Self);
-	};
+class Adapter : public itk::ImportImageContainer<size_t,float>, data::ValueArray<float>{
+public:
+	typedef Adapter Self;
+	typedef itk::ImportImageContainer<size_t,float> Superclass;
+	typedef itk::SmartPointer< Self >       Pointer;
+	typedef itk::SmartPointer< const Self > ConstPointer;
+
+	void SetContainerManageMemory (bool _arg)override{
+		LOG_IF(_arg,Debug,warning) << "Ignoring command to manage container memory";
+	}
+	bool GetContainerManageMemory () const override{
+		return false;
+		
+	}
+ 	void ContainerManageMemoryOn ()override{
+		
+	}
+	void ContainerManageMemoryOff ()override{
+		
+	}
+	
+	  /** Method for creation through the object factory. */
+	itkNewMacro(Self);
+	
+	void setSource(const data::ValueArray<float> &src){//@todo should not be const
+		data::ValueArray<float>::operator=(src);
+		SetSize(src.getLength());
+		SetCapacity(src.getLength());
+	}
+	
+protected:
+	float* 	AllocateElements(size_t size, bool /*UseDefaultConstructor*/) const override{
+		LOG_IF(size,Debug,warning) << "Ignoring command to allocate elements";
+	}
+	void DeallocateManagedMemory ()override{
+		LOG(Debug,warning) << "Deallocating ValueArray";
+		SetSize(0);
+	}
+};
+
 }
 
-template<typename TYPE> class IsisITKImage : public itk::Image<TYPE,4>
+class IsisITKChunk : public itk::Image<float,4>
 {
 public:
-	IsisITKImage(data::TypedChunk<TYPE> &src){
-		auto container=_internal::ContainerAdapter<TYPE>::New();
-		container.SetImportPointer(src.begin(),src.getVolume(),false);
+	IsisITKChunk(const data::TypedChunk<float> &src){ //@todo should not be const
+		itk::Image<float,4>::IndexType start{0,0,0,0};
+		itk::Image<float,4>::SizeType  size{src.getDimSize(0),src.getDimSize(1),src.getDimSize(2),src.getDimSize(3)};
+		itk::Image<float,4>::RegionType region;
 		
-		itk::Image<TYPE,4>::SetPixelContainer(container);
+		region.SetSize( size );
+		region.SetIndex( start );
+
+		auto container=_internal::Adapter::New();
+		container->setSource(src.getValueArray<float>());
+		
+		itk::Image<float,4>::SetPixelContainer(container);
+		itk::Image<float,4>::SetRegions( region );
+		itk::Image<float,4>::ComputeOffsetTable();
+		
 	}
 };
 
--- tools/isistransform.cpp
+++ tools/isistransform.cpp
@@ -1,6 +1,8 @@
 #include <isis/data/io_application.hpp>
 #include <isis/data/io_factory.hpp>
 #include <isis/math/transform.hpp>
+#include <isis/adapter/itk/common.hpp>
+#include <isis/adapter/itk/common.hxx>
 
 #include <map>
 #include <boost/assign.hpp>
@@ -15,48 +17,56 @@ int main( int argc, char **argv )
 {
 	ENABLE_LOG( data::Runtime, util::DefaultMsgPrint, error );
 	
-	util::Selection face("sagittal,coronal,axial");
-	
-	util::Selection x("x,y,z,x-,y-,z-","x"),y("x,y,z,x-,y-,z-","y"),z("x,y,z,x-,y-,z-","z");
-	
-	util::Selection mode( "image,space,both","both" );
 
 	data::IOApplication app( "isistransform", true, true );
-	app.parameters["face"] = face;
-	app.parameters["face"].needed() = false;
+	app.parameters["swapdim"] = util::slist{"x","y","z", "t"};
+	app.parameters["swapdim"].needed()=false;
 	
-	app.parameters["x"]=x;
-	app.parameters["x"].needed()=false;
-	app.parameters["y"]=y;
-	app.parameters["y"].needed()=false;
-	app.parameters["z"]=z;
-	app.parameters["z"].needed()=false;
-
-	app.parameters["mode"] = mode;
-	app.parameters["mode"].needed() = false;
-	app.parameters["mode"].setDescription( "What should be transformed" );
+	app.parameters["resample"]=util::ivector4{-1,-1,-1,-1};
+	app.parameters["resample"].needed()=false;
 	
 	app.addLogging<TransformLog>();
 	app.addLogging<TransformDebug>();
 	
 	app.init( argc, argv );
 	
-	face=app.parameters["face"];
-	util::Selection dims[]={app.parameters["x"],app.parameters["y"],app.parameters["z"]};
-
+	util::slist swapdim_list=app.parameters["swapdim"];
+	LOG_IF(swapdim_list.size()>4,TransformLog,warning)	<< "Ignoring all but 4 given parameters for swapdim";
+	swapdim_list.resize(4);
+	
 	//go through every image
 	for( data::Image & refImage :  app.images ) {
-		for(int i=0;i<3;i++){
-			unsigned short dim=dims[i]-1;
-			if(dim>3){
-				LOG(TransformLog,info) << "flipping dim " << dim%3;
-				dim%=3;
+		
+		if(app.parameters["swapdim"].isParsed()){
+			int dim=0;
+			for(std::string swap:swapdim_list){
+				int idx=0;bool flip=false;
+				if(swap[idx]=='-'){
+					flip=true;
+					idx++;
+				} else if(swap[idx]=='+'){
+					idx++;
+				} 
+				int target=std::tolower(swap[idx])-'x';
+				if(swap[idx]=='t')target=3;
+				if(target<0 || target>3){
+					target=dim;
+					LOG(TransformLog,warning) << "Ignorig unknown swapdim parameter " << swap;
+					continue;
+				}
+				
+				LOG(TransformLog,info) << "swapping dim " << dim << " and " << target;
+				refImage.swapDim(dim,target, app.feedback());
 			}
-			if(dim!= i){
-				LOG(TransformLog,info) << "swapping dim " << i << " and " << dim;
-				refImage.swapDim(i,dim, app.feedback());
-				std::swap(dims[i],dims[dim]);
+		}
+		if(app.parameters["resample"].isParsed()){
+			util::vector4<size_t> oldsize=refImage.getSizeAsVector(),newsize;
+			util::ivector4 reqsize=app.parameters["resample"];
+			for(int i=0;i<4;i++){
+				newsize[i]=(reqsize[i]!=-1)?
+					reqsize[i]:oldsize[i];
 			}
+			refImage=itk4::resample<float>(refImage,newsize);
 		}
 	}
 	app.autowrite( app.images );
